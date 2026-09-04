bd934bf77e0366eb8e0e0c5eb322329548da9905
rafaelffs <rafaelffs@users.noreply.github.com>
2026-09-04 09:16:08 +0200

    Wire Watch App entry point to FilterMenuView, remove stock ContentView

--- mobile/ios/Watch App Watch App/Watch_AppApp.swift
+++ mobile/ios/Watch App Watch App/Watch_AppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Watch_App_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FilterMenuView()
         }
     }
 }
